0a47dcdabfd9ea57484ad7064693c55d4501abc8
Drahcir Blanco <blancogarciadrahciralexander@gmail.com>
2026-04-13 09:49:09 -0300

    Update talkmap.py

--- talkmap.py
+++ talkmap.py
@@ -18,7 +18,7 @@ TIMEOUT = 5
 g = glob.glob("_talks/*.md")
 
 # Prepare to geolocate
-geocoder = Nominatim(user_agent="academicpages.github.io")
+geocoder = Nominatim(user_agent="bl4nco7.github.io/drahcir//") # academicpages.github.io
 location_dict = {}
 location = ""
 permalink = ""
